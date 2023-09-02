1c858e236ff2b130a1e84eb99f91ca457301499b
HND <hans.doderlein99@gmail.com>
2023-09-02 00:26:33 -0400

    sdvsdsd

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 //sets up connection to database
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
   dialect: "mysql",
   logging: true, // Set to true for debugging
 });
