ed9e908fe52e4ad183d84f70008f0500be71af0c
HND <hans.doderlein99@gmail.com>
2023-09-01 23:45:32 -0400

    sequlize setup changed

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,9 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 //sets up connection to database
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 8000,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "mysql",
+  logging: true, // Set to true for debugging
+});
 
 module.exports = sequelize;
