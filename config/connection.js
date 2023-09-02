fe30e44b154f23a09052d82d0c67ca4172bf13b6
HND <hans.doderlein99@gmail.com>
2023-09-02 00:15:57 -0400

    dcs

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,12 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 //sets up connection to database
-const sequelize = new Sequelize("cleardb - colorful - 17451", {
-  dialect: "mysql",
-  logging: true, // Set to true for debugging
-});
+const sequelize = new Sequelize(
+  "mysql://b2a4db455f0aea:6572ce31@us-cdbr-east-06.cleardb.net/heroku_a8d218e01dc7faf?reconnect=true",
+  {
+    dialect: "mysql",
+    logging: true, // Set to true for debugging
+  }
+);
 
 module.exports = sequelize;
