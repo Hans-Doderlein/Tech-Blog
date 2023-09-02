0477050b099fd03a2fbfb1ff917a330e3598133e
HND <hans.doderlein99@gmail.com>
2023-09-02 00:18:40 -0400

    dfcascaS

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,9 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 //sets up connection to database
-const sequelize = new Sequelize(
-  "mysql://b2a4db455f0aea:6572ce31@us-cdbr-east-06.cleardb.net/heroku_a8d218e01dc7faf?reconnect=true",
-  {
-    dialect: "mysql",
-    logging: true, // Set to true for debugging
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "mysql",
+  logging: true, // Set to true for debugging
+});
 
 module.exports = sequelize;
